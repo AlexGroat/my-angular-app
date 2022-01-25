c23e44b50546f732699fd3709a712cec542e36c5
Alex Groat <alex.groat.95@gmail.com>
2022-01-25 15:40:34 +1030

    adding content for the recipe component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes= [];
+  recipes: Recipe[]= [
+    // new recipe, add a name, description and an image path as parameters
+    new Recipe('A Test recipe', 'This is my test recipe', 'https://img.bestrecipes.com.au/R_L8NQYW/w643-h428-cfill-q90/br/2018/05/nannas-golden-sausage-casserole-recipe-522926-1.jpg' )
+  ];
+  // import the recipe model
   constructor() { }
 
   ngOnInit(): void {
