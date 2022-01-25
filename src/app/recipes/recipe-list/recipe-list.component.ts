263529cbb51097b618e7fc22e07eddfee3d3401d
Alex Groat <alex.groat.95@gmail.com>
2022-01-25 15:43:14 +1030

    populating recipe component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -4,17 +4,19 @@ import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[]= [
+  recipes: Recipe[] = [
     // new recipe, add a name, description and an image path as parameters
-    new Recipe('A Test recipe', 'This is my test recipe', 'https://img.bestrecipes.com.au/R_L8NQYW/w643-h428-cfill-q90/br/2018/05/nannas-golden-sausage-casserole-recipe-522926-1.jpg' )
+    new Recipe(
+      'A Test recipe',
+      'This is my test recipe',
+      'https://img.bestrecipes.com.au/R_L8NQYW/w643-h428-cfill-q90/br/2018/05/nannas-golden-sausage-casserole-recipe-522926-1.jpg'
+    ),
   ];
   // import the recipe model
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
